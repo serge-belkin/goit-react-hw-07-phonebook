796345c0b6a1d002ce800c762e80ec2e8bac7a29
serge-belkin <cer_fi@yahoo.com>
2023-02-01 08:51:40 +0200

    add operations

--- src/index.js
+++ src/index.js
@@ -4,17 +4,11 @@ import { App } from 'components/App';
 import './index.css';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import persistStore from 'redux-persist/es/persistStore';
-
-let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
         <App />
-      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,8 @@
-import storage from "redux-persist/lib/storage";
-import persistReducer from 'redux-persist/es/persistReducer'
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { filterReducer } from "./filterSlice";
 import { contactsReducer } from "./contactsSlice";
-
-const persistConfig = {
-  key: 'root',
-  storage: storage,
-  whitelist: ['contacts'],
-};
-
-const persistedReducer = persistReducer(
-  persistConfig,
-  combineReducers({
-    contacts: contactsReducer,
-    filter: filterReducer,
-  })
-);
+import { configureStore } from '@reduxjs/toolkit';
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+  reducer: { contacts: contactsReducer, filter: filterReducer },
 });
+
